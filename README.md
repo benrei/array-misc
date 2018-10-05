90b26da1db27de5c3ba0752673b8f68d85a7a27c
Benjamin Reiersølmoen <benjamin.reiersolmoen@traftec.no>
2018-10-05 14:31:08 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # array-misc
+
+[![NPM version](https://img.shields.io/npm/v/array-misc.svg)](https://www.npmjs.com/package/array-misc)
+
 Array utils
+
+- Shifts array element up, down or custom
+- Find common values in two arrays
+- Find not common values in two arrays
+- Find array values not found in other array
+
+
+## Installation
+
+```sh
+$ npm install array-misc
+```
+
+## Usage
+```js
+var arrayMisc = require('array-misc');
+
+var arr = [1,2,3,4,5];
+//  up(array, index)
+arrayMisc.down(arr, 0);
+console.log(arr); // -> [2,1,3,4,5]
+
+//  down(array, index)
+arrayMisc.up(arr, 0);
+console.log(arr); // -> [1,2,3,4,5]
+
+//  custom(array, fromIndex, toIndex)
+arrayMisc.custom(arr, 0, 4)
+console.log(arr); // -> [2,3,4,5,1]
+
+
+
+var array1 = ["arr1", "arr2","common3","arr3", "common1", "common2", "arr4", "arr5", "arr6"];
+var array2 = ["arrOther1", "arrOther2","common1", "common2", "common3", "extra"];
+
+arrayMisc.findCommonValues(array1,array2);
+arrayMisc.findNotCommonValues(array1,array2);
+arrayMisc.findArrValuesNotFoundInOtherArr(array1,array2);
+arrayMisc.findArrValuesNotFoundInOtherArr(array2,array1);
+
+//  Output
+//  ["common1", "common2", "common3"]
+//  ["arr1", "arr2", "arr3", "arr4", "arr5", "arr6", "arrOther1", "arrOther2", "extra"]
+//  ["arr1", "arr2", "arr3", "arr4", "arr5", "arr6"]
+//  ["arrOther1", "arrOther2", "extra"]
+```
+
+
+## License
+
+  [MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict";
+
+function shift(array, old_index, new_index) {
+  if (new_index >= array.length) {
+    var k = new_index - array.length + 1;
+    while (k--) {
+      array.push(undefined);
+    }
+  }
+  array.splice(new_index, 0, array.splice(old_index, 1)[0]);
+}
+
+function down(array, index) {
+  if (array && index >= 0 && index < array.length - 1) shift(array, index, index + 1);
+}
+
+function up(array, index) {
+  if (array && index > 0) shift(array, index, index - 1);
+}
+
+
+function findCommonValues(arr, otherArr, sort = false) {
+  var ret = [];
+  for(var i in arr) {
+    if(otherArr.indexOf(arr[i]) > -1){
+      ret.push(arr[i]);
+    }
+  }
+  if(sort) return ret.sort();
+  return ret;
+}
+
+function findNotCommonValues(arr, otherArr, sort = false) {
+  var ret = [];
+  for(var i in arr) {
+    if(otherArr.indexOf(arr[i]) === -1){
+      ret.push(arr[i]);
+    }
+  }
+  for(var i in otherArr) {
+    if(arr.indexOf(otherArr[i]) === -1){
+      ret.push(otherArr[i]);
+    }
+  }
+  if(sort) return ret.sort();
+  return ret;
+}
+
+function findArrValuesNotFoundInOtherArr(arr, otherArr, sort = false) {
+  var ret = [];
+  for(var j in arr) {
+    if(otherArr.indexOf(arr[j]) === -1){
+      if(arr[j]) ret.push(arr[j]);
+    }
+  }
+  if(sort) return ret.sort();
+  return ret;
+}
+
+module.exports.shift = shift;
+module.exports.down = down;
+module.exports.up = up;
+module.exports.findCommonValues = findCommonValues;
+module.exports.findNotCommonValues = findNotCommonValues;
+module.exports.findArrValuesNotFoundInOtherArr = findArrValuesNotFoundInOtherArr;
